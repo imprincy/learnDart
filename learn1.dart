5629b3b9fad15112c5406c3cb565a1a3101aea54
shivamsharma <shivams1110@live.in>
2023-05-15 18:34:08 +0530

    learn first dart

---
+++ learn1.dart
@@ -0,0 +1,18 @@
+void main() {
+  /*print('Hello World!');
+  print("Hello City")*/
+  ;
+
+  ///Doc
+  // String name = "Princy";
+  // print(name);
+  // int age = 10;
+  // print(age);
+  // List arr = [1, 2, "Shivam"];
+  // print(arr);
+  // print(arr[2]);
+  // Map arrs= {'Name': 'Princy', 'City': 'Noida'};
+  // print(arrs['City']);
+  var name = " Princy ";
+  print(name.trim().length);
+}
