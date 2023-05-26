9eea8c85dfb42a77145cb66857f8ed487c1658a7
shivamsharma <shivams1110@live.in>
2023-05-26 22:43:34 +0530

    adding list.dart and listoperations.dart

---
+++ listoperations.dart
@@ -0,0 +1,11 @@
+void main() {
+  var Number = [10, 20, 30, 40, 50, 60];
+  print('Length: ${Number.length}');
+  print('Reversed:${Number.reversed}');
+  print('Is Empty: ${Number.isEmpty}');
+  print('Is Not Empty: ${Number.isNotEmpty}');
+  print('2nd Index Element: ${Number.elementAt(2)}');
+  print('First: ${Number.first}');
+  print('Last: ${Number.last}');
+
+}
